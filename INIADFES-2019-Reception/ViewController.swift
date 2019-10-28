1818b0edd367896679b3f91efcd834e4d0022a08
Kentaro <kentaro@whim.jp>
2019-10-28 12:41:31 +0900

    Initial Commit

---
+++ INIADFES-2019-Reception/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  INIADFES-2019-Reception
+//
+//  Created by Kentaro on 2019/10/28.
+//  Copyright © 2019 Kentaro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
